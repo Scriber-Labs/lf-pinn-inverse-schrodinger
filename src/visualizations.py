486ef3832a37c09217e0ae60b613b09e67cc954c
Sofia Scriber <sofiashriver@gmail.com>
2026-02-13 22:51:32 -0500

    Minor fixes.

--- src/visualizations.py
+++ src/visualizations.py
@@ -681,7 +681,7 @@ def plot_pod_singular_values(
         np.arange(1, len(sv) + 1),
         sv,
         marker="o",
-        color="#007FFF",
+        color="#8000FF",
         linewidth=2,
         label="Singular values",
     )
